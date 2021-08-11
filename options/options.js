e534b42e26d0afdd1512ea6549f201717c8d5fda
Crowdbotics <team@crowdbotics.com>
2021-08-11 16:10:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sparkling_term_29723Identifier",
+  url: "https://sparkling_term_29723Identifier.botics.co",
+  api: "https://sparkling_term_29723Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
